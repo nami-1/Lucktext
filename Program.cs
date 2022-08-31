7870f7218101617fcdd4e11ce8f134dea4aa1dbe
Namith Marasinha <namith.marasinhamudiyanselage@elev.ga.ntig.se>
2022-08-31 14:26:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string name;
+string age;
+string sport;
+string dricka;
+
+
+Console.WriteLine("Hej! Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine($"Välkommen {name} ! ");
+
+Console.WriteLine("Hur gammal är du?");
+age = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit sport?");
+sport = Console.ReadLine();
+
+Console.WriteLine("Skriv din favorit dricka!");
+dricka = Console.ReadLine();
+
+
+Console.WriteLine($"Hej du! {name} är {age} gammal, och har spelat mycket {sport} och nu behöver dricka {dricka}.");
+
+Console.ReadLine();
+
