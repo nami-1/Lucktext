29c54b81449ea512a2d660301966593de5c75a8c
Namith Marasinha <namith.marasinhamudiyanselage@elev.ga.ntig.se>
2022-09-08 13:55:17 +0200

    Interactive_story

---
+++ null/Interactive_Story/Interactive_Story/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.BackgroundColor = ConsoleColor.Magenta;
+
+
+Console.WriteLine(" Hello, what is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}");
+
+Console.WriteLine("You came to the end of the road and now need to choose a path, which path are you choosing right or left?");
+
+string path = Console.ReadLine();
+
+
+
+if (path == "right"){
+   Console.WriteLine("Now you could decide which drink you enjoy the most fanta or sprite ?");
+
+   string drink = Console.ReadLine();
+
+   if (drink == "fanta") {
+      Console.WriteLine("Fanta is a good decision! (End of the story)");
+   }
+
+   else if (drink == "sprite"){
+      Console.WriteLine("Sprite is a good decsion! (End of the story)");
+   }
+
+}
+
+
+else if (path == "left"){
+     Console.WriteLine("Now choose between these two meals: pizza or hamburger");
+
+     String food = Console.ReadLine();
+
+     if (food == "pizza"){
+      Console.WriteLine("Pizza is better right? (End of the story)");
+     }
+
+     else if (food == "hamburger"){
+      Console.WriteLine("Hamburger over pizza? (End of the story)");
+     }
+
+}
+
+
+
+
+Console.ReadLine();
+
+
+
